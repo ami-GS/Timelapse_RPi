7ad74d8952878a814eeb3483fba14dd0759f2796
Daiki Aminaka <1991.daiki@gmail.com>
2014-05-09 01:22:17 +0900

    ajust to html5 slider parameter

--- imageprocess.py
+++ imageprocess.py
@@ -4,7 +4,13 @@ import cv2
 class imageProcess():
     def __init__(self):
         self.prev = ""
+        self.param1 = 100
+        self.param2 = 200
         pass
+
+    def setParam(self, param1=100, param2=200):
+        self.param1 = param1
+        self.param2 = param2
 
     def assign(self, img, process):
         if process == "normal":
@@ -21,7 +27,7 @@ class imageProcess():
         return img
 
     def edgeDetect(self, img):
-        img = cv2.Canny(img, 100, 200)
+        img = cv2.Canny(img, self.param1, self.param2)
         return img
 
     def grayImage(self, img):
@@ -30,7 +36,9 @@ class imageProcess():
         return img
 
     def motionDetect(self, img):
-        tmp = cv2.absdiff(self.prev, img)
-        tmp = cv2.threshold(tmp, 40, 255, cv2.cv.CV_THRESH_BINARY)[1]
+        tmp1 = cv2.cvtColor(self.prev, cv2.cv.CV_RGB2GRAY)
+        tmp2 = cv2.cvtColor(img, cv2.cv.CV_RGB2GRAY)
+        tmp = cv2.absdiff(tmp1, tmp2)
+        tmp = cv2.threshold(tmp, self.param1, self.param2, cv2.cv.CV_THRESH_BINARY)[1]
         self.prev = img
         return tmp
