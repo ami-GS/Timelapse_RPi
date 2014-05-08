d24d0ac354fd8fbf8ca277f08421f4e36cee8b42
Daiki Aminaka <1991.daiki@gmail.com>
2014-05-08 20:50:36 +0900

    added 3 processing function

--- imageprocess.py
+++ imageprocess.py
@@ -1,13 +1,36 @@
 import cv2
 
+
 class imageProcess():
-    def __init__(self, size):
-        self,size = size
-
-    def edgeDetect(self):
-        #canny
+    def __init__(self):
+        self.prev = ""
         pass
 
-    def motionDetect(self):
-        #difference
-        pass
+    def assign(self, img, process):
+        if process == "normal":
+            self.prev = img
+            return img
+
+        if process == "edge":
+            img = self.edgeDetect(img)
+        elif process == "gray":
+            img = self.grayImage(img)
+        elif process == "motion":
+            img = self.motionDetect(img)
+
+        return img
+
+    def edgeDetect(self, img):
+        img = cv2.Canny(img, 100, 200)
+        return img
+
+    def grayImage(self, img):
+        img = cv2.cvtColor(img, cv2.cv.CV_RGB2GRAY)
+        img = cv2.cvtColor(img, cv2.cv.CV_GRAY2RGB)
+        return img
+
+    def motionDetect(self, img):
+        tmp = cv2.absdiff(self.prev, img)
+        tmp = cv2.threshold(tmp, 40, 255, cv2.cv.CV_THRESH_BINARY)[1]
+        self.prev = img
+        return tmp
