22c8baf8f31736001c59c1e33f57bc382c619892
Daiki Aminaka <1991.daiki@gmail.com>
2014-06-25 21:15:04 +0900

    remove os.chdir to avoid server error

--- makevideo.py
+++ makevideo.py
@@ -13,24 +13,22 @@ class makeVideo():
         self.ZFILL = ZFILL
 
     def ffmpeg(self):
-        os.chdir(self.DIRNAME)
         if platform.system() == "Darwin":
             subprocess.call(["ffmpeg", "-r", "%f" % self.ENCODEFPS,
                              "-i", "%%0%dd.jpg" % self.ZFILL,
                              "-vcodec", "libx264", "-q", "0", "-vf",
                              "scale=1620:1080,pad=1920:1080:150:0,setdar=16:9",
-                             "%s-%dfps.mp4" % (self.DIRNAME, self.FPS)]) # for another platform
+                             "%s/%s-%dfps.mp4" % (self.DIRNAME, self.DIRNAME, self.FPS)]) # for another platform
         else:
             subprocess.call(["ffmpeg", "-r", "%f" % self.ENCODEFPS,
                              "-i", "%%0%dd.jpg" % self.ZFILL,
                              "-vcodec", "libx264", "-sameq", "-vf",
                              "scale=1620:1080,pad=1920:1080:150:0,setdar=16:9",
-                             "%s-%dfps.mp4" % (self.DIRNAME, self.FPS)]) # for RPi
+                             "%s/%s-%dfps.mp4" % (self.DIRNAME, self.DIRNAME, self.FPS)]) # for RPi
 
     def initWriter(self, size):
-        os.chdir(self.DIRNAME)
         self.writer = cv2.VideoWriter()
-        success = self.writer.open("%s-%dfps.mp4" % (self.DIRNAME, self.FPS),
+        success = self.writer.open(self.DIRNAME+"/%s-%dfps.mp4" % (self.DIRNAME, self.FPS),
                                    FOURCC, self.ENCODEFPS, size, True)
 
     def write(self, frame):
